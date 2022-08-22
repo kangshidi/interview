d135faa00738db06ca0dc0f07c8c493be18d3dd3
kangshidi <yujingyao@126.com>
2022-08-22 14:54:47 +0800

    Create throttle.js

---
+++ throttle.js
@@ -0,0 +1,19 @@
+/* 
+ * 节流 单位时间执行一次
+ */
+function throttle(fn, timeout = 1000) {
+  let timer;
+  let context;
+  return () => {
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      fn.apply(context, arguments);
+      timer = null; // 十分重要
+    }, timeout);
+  }
+}
+window.addEventListener("resize", throttle(() => {
+  console.log('--- resize');
+}));
